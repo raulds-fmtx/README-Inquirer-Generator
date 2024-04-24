485a14ca3e86c43713de55a12a25e87773c54d5c
raulds-fmtx <rdsflomo03@gmail.com>
2024-04-23 20:56:57 -0500

    packages added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [];
